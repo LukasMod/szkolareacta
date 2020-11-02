0eabbddd2379819c926e6c8c3ef9c2accd38b027
Lukasz Modzelewski <lukaszmod@o2.pl>
2020-11-02 09:40:55 +0100

    fix: list of homeworks in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '03/06/App'; //CHANGE WEEK HERE
+import App from '03/06/App';
+
+//change exercise in App import
+// -----------
+// 1 week:
+// 01
+// -----------
+// 2 week:
+// 02/01
+// 02/02
+// 02/03
+// -----------
+// 3 week:
+// 03/01_03
+// 03/04
+// 03/05
+// 03/06
 
 ReactDOM.render(
   <React.StrictMode>
