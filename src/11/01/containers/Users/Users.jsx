615376b22ca8a2c2baceeea475c39cd07008e992
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-01-31 09:44:22 +0100

    add materail-UI to Users component

--- src/11/01/containers/Users/Users.jsx
+++ src/11/01/containers/Users/Users.jsx
@@ -1,5 +1,12 @@
-import { getDefaultNormalizer } from '@testing-library/react';
 import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
 
 const USERS_NUMBER = 10;
 
@@ -17,13 +24,37 @@ const Users = () => {
     getData();
   }, []);
 
+  const useStyles = makeStyles(() => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      textAlign: 'center',
+    },
+    listItemText: {
+      textAlign: 'center',
+    },
+  }));
+  const { root, listItemText } = useStyles();
+
   return (
-    <div>
-      <h1>Users</h1>
-      {users.map((item) => {
-        return <div key={item.login.uuid}>{item.name.first}</div>;
-      })}
-    </div>
+    <Box className={root}>
+      <Typography component="h3" variant="h3">
+        Users
+      </Typography>
+
+      <List component="nav" aria-label="main mailbox folders">
+        {users.map((item) => {
+          return (
+            <ListItem key={item.login.uuid}>
+              <ListItemText
+                primary={item.name.first}
+                className={listItemText}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
+    </Box>
   );
 };
 
