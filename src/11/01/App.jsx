43a6f4721f248487142277416626bbcdfa5ba4a4
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-02-09 22:24:05 +0100

    redux-persist

--- src/11/01/App.jsx
+++ src/11/01/App.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { SnackbarProvider } from 'notistack';
 import store from './store/store';
 import Header from './containers/Header/Header';
 import Content from './containers/Content/Content';
 
+const persistor = persistStore(store);
+
 const App = () => {
   store.subscribe(() => console.log('REDUX STATE', store.getState()));
+
   return (
     <div className="App">
       <Provider store={store}>
-        <Router>
-          <SnackbarProvider maxSnack={3}>
-            <Header />
-            <Content />
-          </SnackbarProvider>
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <SnackbarProvider maxSnack={3}>
+              <Header />
+              <Content />
+            </SnackbarProvider>
+          </Router>
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/11/01/store/store.js
+++ src/11/01/store/store.js
@@ -1,7 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './../reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [thunk];
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['users'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
 export default store;
